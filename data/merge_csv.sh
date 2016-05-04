efbfcebb9a0379b078398b0dfd9135794c4951ab
cbruderer <claudio.bruderer@gmail.com>
2016-05-04 17:46:05 +0200

    Added all the data, a fake completeness, the newest version of the analysis notebook, and a notebook to get the proper completeness

--- data/merge_csv.sh
+++ data/merge_csv.sh
@@ -1,5 +1,5 @@
 array=(africa allah apple arab asylum attack banana birds bitcoin black boston christian climate coffee comet crimea crowded curiosity divorce doomsday ebola epidemic ethiopia europe facebook family flu foreigners galaxy geneva gluten god google guinea higgs holidays homework hurricane immigration influenza iphone isis islam italy jogging left leone liberia linux london mandela marathon marriage maya meat mh17 mh370 migrants nelson nsa obama olympics pasta pc philippines pig pope putin refugees right rosetta russia sandy school sierra snowden sochi sports spring swine syria tea teacher television terrorism twitter ukraine unemployment unhcr usa vaccine vegan vegetarian virus watch wedding whatsapp white who work)
-months=(05 12)
+months=(05 06 07 09 10 11 12)
 
 for a in ${array[@]}; do
 	rm ${a}.csv
